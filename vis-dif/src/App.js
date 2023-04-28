67fbbc97a3b441093405ff263582747264d9befd
rubyy-y <rubinawaldbauer@gmail.com>
2023-04-28 15:25:36 +0200

    handleChange works
    finally

--- vis-dif/src/App.js
+++ vis-dif/src/App.js
@@ -1,27 +1,40 @@
 import './App.css';
-import React, {useState} from "react";
+import React, { useState } from "react";
 import vegaEmbed from 'vega-embed';
 
 function App() {
   const [select, setSelect] = useState();
 
-vegaEmbed('#vis1', 'data/iris_source.json');
-vegaEmbed('#vis2', 'data/iris20_source.json');
-vegaEmbed('#dif', 'data/iris_COMP_iris20.json');
+  const data = {
+    anscombe: 'data/anscombe_source.json',
+    barley: 'data/barley_source.json',
+    burtin: 'data/burtin_source.json',
+    cars: 'data/cars_source.json',
+    crimea: 'data/crimea_source.json',
+    driving: 'data/driving_source.json',
+    iris: 'data/iris_source.json',
+    ohlc: 'data/ohlc_source.json',
+    wheat: 'data/wheat_source.json',
+  };
+
+  const handleChange = (e) => {
+    setSelect(e.target.options[e.target.selectedIndex].value);
+  };
+
+  vegaEmbed('#vis1', data[select]);
+  vegaEmbed('#vis2', 'data/iris20_source.json');
+  vegaEmbed('#dif', 'data/iris_COMP_iris20.json');
 
   return (
     <div className="App">
-      
       <header className="App-header">
-        <p>
-          Highlighting Differences in Data Visualizations
-        </p>
+        <p>Highlighting Differences in Data Visualizations</p>
       </header>
 
-      <body>
+      <div className="body">
         <div className="dropdown">
           <label for="datasets">Choose a dataset: </label>
-          <select value={select} onChange={e=>setSelect(e.target.value)}>
+          <select value={select} onChange={handleChange}>
             <option value="anscombe">anscombe.json</option>
             <option value="barley">barley.json</option>
             <option value="burtin">burtin.json</option>
@@ -56,7 +69,7 @@ vegaEmbed('#dif', 'data/iris_COMP_iris20.json');
         <div id="dif" className="dif">
           This is where a visualization of the differences will appear.
         </div>
-      </body>
+      </div>
     </div>
   );
 }
