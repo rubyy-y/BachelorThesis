f08540cfde92ef61fd77adf2f83063b656a6733a
rubyy-y <rubinawaldbauer@gmail.com>
2023-05-15 16:40:11 +0200

    updated comparisons
    for file sensitive encodings

--- compare.py
+++ compare.py
@@ -2,8 +2,9 @@
 import os
 import json
 
-
 def compare(a_json, b_json):
+    print(a_json, "and", b_json)
+
     with open(a_json) as a, open(b_json) as b:
         a_vl = json.load(a)
         b_vl = json.load(b)
@@ -56,12 +57,12 @@ def compare(a_json, b_json):
         hash_ = hash_a
         mark = a_vl["mark"]
         encoding = a_vl["encoding"]
-        encoding["color"]["field"] = "file of origin"
-        encoding["color"]["type"] = "nominal"
-        encoding["color"]["scale"] = {
-            "domain": ["File 1", "File 2"],
-            "range": ["lightblue", "gold"]
-        }
+        # encoding["color"]["field"] = "file of origin"
+        # encoding["color"]["type"] = "nominal"
+        # encoding["color"]["scale"] = {
+        #     "domain": ["File 1", "File 2"],
+        #     "range": ["lightblue", "gold"]
+        # }
 
         output_vl = {
             "width": "container",
@@ -84,24 +85,31 @@ def compare(a_json, b_json):
             }
         }
 
-        # specs - optional
-        # selector
+        # specs - optional selector
         try:
             selection = a_vl["selection"]
             output_vl["selection"] = selection
         except:
             pass
 
-
-        # save with original color coding
+        # save with file coloring
         fe = len('_source.json')    # length of file ending
         output_file = f"{a_json[:-fe]}_COMP_{b_json[:-fe]}.json"
         with open("comparisons/" + output_file, 'w') as out:
             json.dump(output_vl, out, indent=3)
 
-        # save with file coloring
+        # save with file color encoding
+        output_vl["encoding"]["color"]["field"] = "file of origin"
+        output_vl["encoding"]["color"]["type"] = "nominal"
+        output_vl["encoding"]["color"]["scale"] = {
+            "domain": ["File 1", "File 2"],
+            "range": ["lightblue", "gold"]
+        }
+
         with open("comparisons/filecolor/" + output_file, 'w') as out:
             json.dump(output_vl, out, indent=3)
+
+        print("successful\n")
         
 
 # compare all the files in the data folder of our app-directory
@@ -122,10 +130,8 @@ for dataset in datasets:
                 alternation = f"{dataset+str(p)}_source.json"
                 try:
                     compare(original, alternation)
-                    print("successful\n")
                 except:
                     print("!! unsuccessful\n")
-
 
 # main call not needed because this is a utility function, used in backend
 # if __name__ == "__name__":
